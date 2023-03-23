d6be2b83cd0281bc2c0b42172d9b5e9e063ced82
colecrum26 <cwcrumrine@gmail.com>
2023-03-23 13:57:25 -0500

    Giphy trending API

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
-function showGiphs(){
-    fetch()
-    .then(()=>{
+const apiKey = "api_key=4S3qelCPd4CXeS1I0PHxE1IWV2LWEUCY"
+function showGiphs() {
+  fetch(
+    `https://api.giphy.com/v1/gifs/trending?${apiKey}`
+  )
+    .then((response) => {
+      const result = response.json();
 
+      return result;
     })
-    .then(()=>{
-        
+    .then((obj) => {
+      console.log(obj);
     })
+    .catch((err) => {
+      console.log(err);
+    });
 }
+showGiphs();
