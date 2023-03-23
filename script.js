42918368644d959ffd55a3dd5d18a4470365e8ff
colecrum26 <cwcrumrine@gmail.com>
2023-03-23 14:33:08 -0500

    populating webpage with gifs

--- script.js
+++ script.js
@@ -1,15 +1,21 @@
+const dogId = document.getElementById("dogImg");
+const dogId2 = document.getElementById("dogImg2");
+
 const apiKey = "api_key=4S3qelCPd4CXeS1I0PHxE1IWV2LWEUCY"
 function showGiphs() {
   fetch(
-    `https://api.giphy.com/v1/gifs/trending?${apiKey}`
+    `https://api.giphy.com/v1/gifs/search?${apiKey}&q=dogs`
   )
     .then((response) => {
       const result = response.json();
-
       return result;
     })
     .then((obj) => {
-      console.log(obj);
+        const dogImg1 = obj.data[0].images.original.url;
+        const dogImg2 = obj.data[1].images.original.url;
+        console.log(dogImg2);
+        dogId.src = dogImg1;
+        dogId2.src = dogImg2;
     })
     .catch((err) => {
       console.log(err);
